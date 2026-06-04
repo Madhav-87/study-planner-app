9bd5568fb989a0673ad0b1fb4b8561fc3e499787
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-06-04 14:50:28 +0530

    fix error handling : add global error handler

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,6 +10,11 @@ app.use(express.json());
 
 app.use("/",router)
 
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(err.status || 500).json({message:err.stack || "Internal Server Error", success:false});
+})
 app.listen(process.env.SERVER_PORT,()=>{
 console.log(`Server is running on port:${7000}`)
 })
