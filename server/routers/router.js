35ec25d46ffd3de7d00bf2e09516c3afcd2defdd
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-06-17 16:04:16 +0530

    rename folder :routes to routers

---
+++ server/routers/router.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import multer from 'multer';
+import {controller,sendMessage} from '../controllers/controller.js';
+const router=express.Router();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024 // 10MB limit
+  }
+});
+
+router.post('/chatbot/input',upload.single("syllabus"),controller);
+router.get('/',sendMessage);
+export default router;
